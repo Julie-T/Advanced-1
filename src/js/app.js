bd6786c42a708bd6d44c1d679bff80b0c476d6b5
Juliya <yula_sakura@list.ru>
2022-07-18 15:28:21 +0300

    Commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,7 @@
 // TODO: write your code here
 
-export default function orderByProps(obj, [...key]) {
-  const arrOfObj = [];
-  for (const prop in obj) {
-    if (prop === key) {
-      const newObj = {
-        key: this.key,
-        value: this.obj[prop],
-      };
-      arrOfObj.push(newObj);
-    }
-  }
-  return arrOfObj;
+export default function orderByProps(obj, [...keys]) {
+  const newObjArr = [];
+  keys.map((k) => newObjArr.push({ key: k, value: obj[k] }));
+  return newObjArr;
 }
-
-const hero = {
-  name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
-};
-orderByProps(hero, ['name', 'health']);
